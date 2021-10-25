f853837337923e46594f9b02ffbe35e89cf32638
IvanSaavedra <ivansaavedra@appnet.cl>
2021-10-25 12:24:28 -0300

    tema de logo

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
         'public' => [
             'driver' => 'local',
             //'root' => storage_path('app/public'),
-            'root' => public_path().'\img',
+            'root' => public_path().'/img',
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
