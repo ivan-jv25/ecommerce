76c1342e0aca172a642c3dcff5e000c8050bec7d
dino diseno <appnet_diseno@hotmail.com>
2021-10-28 10:47:25 -0300

    una columna

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
         <link href="{{ asset('favicon.ico') }}" rel="icon" type="image/ico">
     </head>
-<body>
+<body class="unacol">
 
     <div class="pop-up">
       <div id="cerrar-popup">
